7eb21d6864fcff581554d39e33a3e6d0964554d8
kalyani <kalyani.nelluri54@gmail.com>
2023-07-17 14:27:05 +0100

    Implemented chuck.card to display greeting msg and image

--- src/components/chuck_card.tsx
+++ src/components/chuck_card.tsx
@@ -2,19 +2,19 @@
 import ChuckImage from '../assets/chuck_norris.jpg';
 
 interface ChuckCardProps {
-
+	greeting:string
 }
 
-const ChuckCard: React.FC<ChuckCardProps> = ({}) => {
+const ChuckCard: React.FC<ChuckCardProps> = ({greeting}) => {
 
 	return (
 		// 💡 This weird-looking `<>` thing is a "Fragment". Each component must have exactly one child.
 		// 💡 As a result, sometimes it is useful to wrap components in Fragments like this:
 		<>
 			{/* 💡 We can insert JavaScript into our JSX using curly braces like the ones below in the h2 */}
-			<h2>{ }</h2>
+			<h2>{greeting }</h2>
 
-			<img src={'Chuck image here!'} alt='A handsome man' />
+			<img src={'https://i.pinimg.com/originals/0b/c2/f4/0bc2f417af8030b3f4390a0b3a1d8357.jpg'} alt='A handsome man' width={200} height={200} />
 		</>
 	)
 }
