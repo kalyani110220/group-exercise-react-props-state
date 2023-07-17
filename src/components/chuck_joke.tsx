760f92dc2e75ace0ab969419181f6e2b72d04a76
kalyani <kalyani.nelluri54@gmail.com>
2023-07-17 17:54:07 +0100

    Implemented Requirement 3 chuck_joke

--- src/components/chuck_joke.tsx
+++ src/components/chuck_joke.tsx
@@ -1,9 +1,14 @@
-interface ChuckJokeProps {
+import React from 'react';
+import Joke from '../joke';
 
+interface ChuckJokeProps {
+    joke: Joke;
 }
 
-const ChuckJoke: React.FC<ChuckJokeProps> = () => <p></p>
+const ChuckJoke: React.FC<ChuckJokeProps> = ({ joke }) => {
+    return <p>{joke.joke}</p>; // Update the property access
+ 
 
-
+};
 
 export default ChuckJoke;
